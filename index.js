043d200ac9ac875112a107fe6bc4d905f46462ab
Gaurav Singh <gaurav.singh323@gmail.com>
2022-06-11 20:47:39 +0530

    Starting the Project Structure

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const PORT = 8000;
+
+
+// Binding Router Folder to use routes
+app.use("/", require("./router"));
+
+
+
+// Starting server
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log("Error in Running Express Server");
+  }
+  console.log(`Server is Running on Port ${PORT}`);
+});
